6f9cc46201d1664348d5f54f5223987b04656900
tothevoid42 <tothevoid42@gmail.com>
2018-10-10 23:57:20 +0300

    repo stats

--- commands.py
+++ commands.py
@@ -2,6 +2,9 @@ import io
 import random as rnd
 from time import sleep
 from datetime import date
+import requests
+import json
+import config as cfg
 
 with open('watch.txt', encoding='utf-8') as fl:
     watch_films = fl.readlines()
@@ -31,5 +34,15 @@ def get_second_part(msg):
 
 def rnd_film():
     num = rnd.randint(0,len(watched_films)-1)
-    msg = ':video_camera: Сегодня фильм: '+watch_films[num]
+    msg = ":video_camera: Today's film: " + watch_films[num]
     return msg
+
+def get_stats():
+    response = requests.get(cfg.repo_url)
+    info = json.loads(response.text)
+    total_commits = len(info)    
+    commit_info = info[0]['commit']['author']
+    date = commit_info['date']
+    name = commit_info['name']
+
+    return 'Total commits: %s. Last commit by %s (%s)' % (total_commits, name, date)
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-import commands
+import commands as cmd
 
+def test_stats():
+    cmd.get_stats()
+
+test_stats()
